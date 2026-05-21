15ee0b06e49be99c43c0de0766dc638d729db5f3
Brennoborgo <borgobrenno@gmail.com>
2026-05-20 22:32:36 -0300

    Add files via upload

---
+++ Aula-15/script.js
@@ -0,0 +1,27 @@
+
+document.writeln ("Olá Mundo!");
+
+document.writeln ("<h2> Bem vindo ao JavaScript </h2>");
+
+window.alert ("Está e uma mensagem alerta!")
+
+document.getElementById("title").innerHTML = "Aprendendo Javascript com o Melhor Professor Da UVV!"
+
+let nome1 = //prompt(" Digite seu Nome:");
+
+document.getElementById("novafrase").innerHTML = " Olá, " + nome + " Seja Bem Vindo so Javascript";
+
+document.getElementById("novafrase").style.color = "red";
+
+document.getElementById("novafrase").style.fontSize = "80px";
+
+document.getElementById("novafrase").style.textAlign = "center";
+
+function exibirmensagem(){
+let nome = document.getElementById("nome").value;
+document.getElementById("novafrase").innerHTML = " Olá, " + nome + " Seja Bem Vindo so Javascript";
+document.getElementById("novafrase").style.color = "red";
+document.getElementById("novafrase").style.fontSize = "50px";
+document.getElementById("novafrase").style.textAlign = "center";
+}
+
